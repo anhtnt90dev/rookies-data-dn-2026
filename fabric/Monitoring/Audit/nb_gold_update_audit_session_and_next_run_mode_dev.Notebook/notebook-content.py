45e74d7af3606798ce8acab4eb6b6bef5cab301f
Le Viet Vinh Phu <vinhphu2209@gmail.com>
2026-06-12 11:11:03 +0700

    feat(pipeline): integrate gold layer with silver in master flow (#33)

---
+++ fabric/Monitoring/Audit/nb_gold_update_audit_session_and_next_run_mode_dev.Notebook/notebook-content.py
@@ -0,0 +1,78 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "14b073f3-0eb9-4315-8d49-155c39392779",
+# META       "default_lakehouse_name": "lh_insurance_dev",
+# META       "default_lakehouse_workspace_id": "21e1cea5-9786-4ce5-aa47-1d8255b69b82",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "14b073f3-0eb9-4315-8d49-155c39392779"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# PARAMETERS CELL ********************
+
+session_id = ""
+batch_id = ""
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+batch_id = int(batch_id)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+error_message = "Gold layer failed. Pipeline will run in RECOVERY mode next time."
+
+safe_error = error_message.replace("'", "''")
+
+spark.sql(f"""
+    UPDATE cfg.next_run_mode
+    SET next_run_mode = 'RECOVERY',
+        batch_id = {batch_id},
+        session_id = '{session_id}',
+        updated_at = current_timestamp()
+    """
+)
+
+spark.sql(f"""
+UPDATE log.audit_session
+SET session_status = 'FAILED',
+    error_code = 'GOLD_LAYER_FAILED',
+    error_message = '{safe_error}',
+    session_finished = current_timestamp(),
+    updated_at = current_timestamp()
+WHERE id = '{session_id}'
+""")
+
+raise Exception(error_message)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
